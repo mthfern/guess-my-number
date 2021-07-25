4efac86484663d4dc77df3cb0993f01ffff03f3e
Matheus Araujo Fernandes <matheus.afernandes@sp.senac.br>
2021-07-25 11:21:26 -0300

    New button visibility, new labels, code reafactoring.

--- script.js
+++ script.js
@@ -1,119 +1,121 @@
 'use strict';
 
-const DOM_RangeMsg = document.querySelector('.between');
+// DOM elements
+const bodyEl = document.body;
+const btnNew = document.querySelector('.again');
+const btnCheck = document.querySelector('.check');
+const guessEl = document.querySelector('.guess');
+const chancesEl = document.querySelector('.chances');
+const scoreEl = document.querySelector('.score');
+const totalScoreEl = document.querySelector('.highscore');
 
 let secretNumber,
   chances,
   score,
   turn,
   totalscore,
-  reset = true,
-  locked = true;
+  newGame = true,
+  playing = true;
+
+//////////////////////////
+// UI Functions
+//////////////////////////
+const changeSecretNumber = (msg, size) => {
+  document.querySelector('.number').textContent = msg;
+  document.querySelector('.number').style.width = size;
+};
 
 const displayMessage = msg => {
   document.querySelector('.message').textContent = msg;
 };
 
-const updateChances = newChances => {
-  chances = newChances;
-  document.querySelector('.chances').textContent = chances;
+const changeBgColor = newColor => {
+  bodyEl.style.backgroundColor = newColor;
 };
 
-const displaySecretNumber = (msg, size) => {
-  document.querySelector('.number').textContent = msg;
-  document.querySelector('.number').style.width = size;
+const refreshScoreDisplay = () => {
+  chancesEl.textContent = chances;
+  scoreEl.textContent = score;
+  totalScoreEl.textContent = totalscore;
 };
 
-const updateTotalScore = newScore => {
-  totalscore = newScore;
-  document.querySelector('.highscore').textContent = totalscore;
-};
+//////////////////////////
+// Game Logic
+//////////////////////////
 
-const updateScore = newScore => {
-  score = newScore;
-  document.querySelector('.score').textContent = score;
-};
-
-const setBackgroundColor = newColor => {
-  document.body.style.backgroundColor = newColor;
-};
-
-
-// Prepare a new round
+// Start new round
 const newRound = () => {
-  if (locked) {
+  playing = true;
+  turn = 1;
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * score) + 1;
 
-    // Reset round values
-    turn = 1;
-    locked = false;
-    updateScore(20);
-
-    // Generate new secret number
-    secretNumber = Math.trunc(Math.random() * score) + 1;
-    
-    // Refresh UI
-    setBackgroundColor('#222');
-    displaySecretNumber('?', '15rem');
-    document.querySelector('.guess').value = '';
-    displayMessage('Start guessing...');
-
-    // If it's a new game, reset chances and total score
-    if (reset) {
-      reset = false;
-      updateChances(8);
-      updateTotalScore(0);
-    }
-
-    // for tests
-    // console.log(secretNumber);
-  }
-};
-
-// End round, check if won/lost
-const endRound = won => {
-
-  // lock game
-  locked = true;
-
-  if (won) {
-    setBackgroundColor('#60b347');
-    displaySecretNumber(secretNumber, '30rem');
-    displayMessage('YOU CHOSE WISELY! 🧙‍♂️');
-    updateChances(chances + 2);
-    updateTotalScore(totalscore + score);
+  // Start new game
+  if (newGame) {
+    newGame = false;
+    chances = 8;
+    totalscore = 0;
   } else {
-
-    // Game over!
-    reset = true;
-    displayMessage(`FINAL SCORE: ${totalscore}`);
-    setBackgroundColor('#e33c3c');
+    chances += 2;
   }
+
+  // Refresh UI
+  changeBgColor('#1D2225');
+  changeSecretNumber('?', '15rem');
+  displayMessage('Start guessing...');
+  refreshScoreDisplay();
+  btnNew.textContent = 'Next Number';
+  btnNew.classList.add('hidden');
+  btnCheck.classList.remove('hidden');
+  guessEl.value = '';
 };
 
-// Again! btn event
-document.querySelector('.again').addEventListener('click', () => newRound());
+// Start new round
+document.querySelector('.again').addEventListener('click', () => {
+  if (!playing) newRound();
+});
 
-// Check! btn event
+// Check number
 document.querySelector('.check').addEventListener('click', () => {
-  if (!locked) {
-    const guess = Number(document.querySelector('.guess').value);
-    if (!guess || guess <= 0 || guess > 20) {
+  if (playing) {
+    const guess = Number(guessEl.value);
+
+    if (typeof guess !== 'number') {
       displayMessage('WRONG INPTUT ❌');
+    } else if (guess === secretNumber) {
+      // Guess is CORRECT!
+      playing = false;
+      totalscore += score;
+
+      // Update UI
+      changeBgColor('#60b347');
+      changeSecretNumber(secretNumber, '30rem');
+      displayMessage('YOU CHOSE WISELY! 🧙‍♂️');
+      refreshScoreDisplay();
+      btnNew.classList.remove('hidden');
+      btnCheck.classList.add('hidden');
     } else {
-      if (guess === secretNumber) {
-        endRound(true);
+      // Guess is WRONG!
+      chances -= 1;
+
+      if (chances > 0) {
+        score -= turn++;
+        displayMessage(guess < secretNumber ? 'TOO LOW! 🔵' : 'TOO HIGH! 🔴');
       } else {
-        updateChances(chances - 1);
-        updateScore(score - turn);
-        turn++;
-        console.log('next turn =>', turn);
-        if (chances > 0) {
-          displayMessage(guess < secretNumber ? 'TOO LOW! 🔵' : 'TOO HIGH! 🔴');
-        } else {
-          updateScore(0);
-          endRound(false);
-        }
+        // GAME OVER
+        playing = false;
+        newGame = true;
+        score = 0;
+
+        // Update UI
+        changeBgColor('#e33c3c');
+        displayMessage(`FINAL SCORE: ${totalscore}`);
+        btnNew.textContent = 'New Game';
+        btnNew.classList.remove('hidden');
+        btnCheck.classList.add('hidden');
       }
+
+      refreshScoreDisplay();
     }
   }
 });
